644a06bdf155133e2ad85600aa90abff264cda42
이재우 <109905562+jw77668@users.noreply.github.com>
2024-12-23 21:42:31 +0900

    Add files via upload

--- santa.py
+++ santa.py
@@ -7,26 +7,16 @@ submission = pd.read_csv('sample_submission.csv')
 
 
 
-print(submission.iloc[3])
 
 #draw the routes
 data_for_draw = submission
-data_for_draw['x'] = data_for_draw['point_id'].map(data['x'])
- 
-
 data = data.set_index('point_id')
 data_for_draw['x'] = data_for_draw['point_id'].map(data['x'])
 data_for_draw['y'] = data_for_draw['point_id'].map(data['y'])
 
 plt.plot(data_for_draw['x'],data_for_draw['y'], marker = '.',color = 'c')
-
-# Labeling the axes
 plt.xlabel('x')
 plt.ylabel('y')
-
-# Add legend to distinguish between the two plots
 plt.legend()
-
-# Show the plot
 plt.show()
 
