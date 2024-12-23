b040d367d4378130bbf399c3881a6f804e835389
이재우 <109905562+jw77668@users.noreply.github.com>
2024-12-23 21:40:47 +0900

    Add files via upload
    draw the routes

--- santa.py
+++ santa.py
@@ -5,16 +5,20 @@ import matplotlib.pyplot as plt
 data = pd.read_csv('data.csv')
 submission = pd.read_csv('sample_submission.csv')
 
-# Filter data for x > 50 and x <= 50
-x_fif = data[data['x'] > 50]  # Filter rows where x > 50
-x_oth = data[data['x'] <= 50] # Filter rows where x <= 50
 
-print(submission)
-# Scatter plot for x > 50
-plt.scatter(x_fif['x'], x_fif['y'], color='red', label='x > 50')
 
-# Scatter plot for x <= 50
-plt.scatter(x_oth['x'], x_oth['y'], color='blue', label='x <= 50')
+print(submission.iloc[3])
+
+#draw the routes
+data_for_draw = submission
+data_for_draw['x'] = data_for_draw['point_id'].map(data['x'])
+ 
+
+data = data.set_index('point_id')
+data_for_draw['x'] = data_for_draw['point_id'].map(data['x'])
+data_for_draw['y'] = data_for_draw['point_id'].map(data['y'])
+
+plt.plot(data_for_draw['x'],data_for_draw['y'], marker = '.',color = 'c')
 
 # Labeling the axes
 plt.xlabel('x')
